e2dc8384d8f8cba8b45dd31ff4ab47c7e0effa1b
Yashgamer-x <yashdesai121212@gmail.com>
2026-01-22 13:12:03 -0500

    Initial Commit

---
+++ LandMarks/LandMarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandMarksApp.swift
+//  LandMarks
+//
+//  Created by Desai, Yash T on 1/22/26.
+//
+
+import SwiftUI
+
+@main
+struct LandMarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
