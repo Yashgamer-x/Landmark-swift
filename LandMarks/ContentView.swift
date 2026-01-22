2155c990dd224057bb38252da7cafd4a00d92b7d
Yashgamer-x <yashdesai121212@gmail.com>
2026-01-22 13:42:17 -0500

    initial commit

--- LandMarks/ContentView.swift
+++ LandMarks/ContentView.swift
@@ -9,13 +9,28 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Image("tom-calling")
+            .clipShape(.circle)
+            .overlay{
+                Circle()
+                    .stroke(
+                        .gray,
+                        lineWidth: 4
+                    )
+                    .shadow(radius: 7)
+            }
+        VStack(alignment: .leading){
+            Text("Turtle Rock")
+                .font(.title)
+            
+            HStack{
+                Text("Joshua Tree National Park")
+                    .font(.subheadline)
+                Spacer()
+                Text("California")
+                    .font(.subheadline)
+            }
         }
-        .padding()
     }
 }
 
