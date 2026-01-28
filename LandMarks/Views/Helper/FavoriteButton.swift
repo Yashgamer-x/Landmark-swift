68fe447331edb4f60a466ba9e8a6a4d883e5a568
Yashgamer-x <yashdesai121212@gmail.com>
2026-01-27 21:37:06 -0500

    Now the I can select the favorite place

---
+++ LandMarks/Views/Helper/FavoriteButton.swift
@@ -0,0 +1,26 @@
+//
+//  FavoriteButton.swift
+//  LandMarks
+//
+//  Created by Desai, Yash T on 1/27/26.
+//
+
+import SwiftUI
+
+struct FavoriteButton: View {
+    @Binding var isSet: Bool
+    
+    var body: some View {
+        Button{
+            isSet.toggle()
+        } label: {
+            Label("Toggle Favorite", systemImage: isSet ? "star.fill" : "star")
+                .labelStyle(.iconOnly)
+                .foregroundStyle(isSet ? .yellow : .gray)
+        }
+    }
+}
+
+#Preview {
+    FavoriteButton(isSet: .constant(true))
+}
